42e25b01de9fd8c2715168001f6eabd2281d8208
Prathik B <prathikreddy560@gmail.com>
2026-07-10 11:01:55 +0530

    Changes done in config and assigned right paths

--- config.py
+++ config.py
@@ -10,15 +10,15 @@ IS_KAGGLE = os.environ.get('KAGGLE_KERNEL_RUN_TYPE') is not None
 if IS_KAGGLE:
     # On Kaggle, datasets are nested under different usernames based on how they were attached
     # Using the official APTOS dataset because the 'paraspatil' one does not contain images
-    APTOS_DIR = "/kaggle/input/aptos2019-blindness-detection"
+    APTOS_DIR = "/kaggle/input/datasets/mariaherrerot/aptos2019"
     IMAGES_DIR = os.path.join(APTOS_DIR, "train_images")
     LABELS_CSV = os.path.join(APTOS_DIR, "train.csv")
     
     # Assuming ODIR is under your username like IDRiD
-    NORMAL_DIR = "/kaggle/input/datasets/lakshmiprathik/odir-dr-dataset/normal" 
+    NORMAL_DIR = "/kaggle/input/datasets/lakshmiprathik/odir-5k" 
     
     # IDRiD dataset
-    IDRID_DIR       = "/kaggle/input/datasets/lakshmiprathik/idrid-dr-dataset/B. Disease Grading"
+    IDRID_DIR       = "/kaggle/input/datasets/lakshmiprathik/idrid-dr-dataset"
     IDRID_TRAIN_IMG = os.path.join(IDRID_DIR, "1. Original Images", "a. Training Set")
     IDRID_TEST_IMG  = os.path.join(IDRID_DIR, "1. Original Images", "b. Testing Set")
     IDRID_TRAIN_CSV = os.path.join(IDRID_DIR, "2. Groundtruths", "a. IDRiD_Disease Grading_Training Labels.csv")
